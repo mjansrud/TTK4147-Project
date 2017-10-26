72a7bb829d3bfb4be814a98f080a4be260c341ad
Ho Doe <Ho@Doe.doe>
2017-10-26 16:32:41 +0200

    Added some functions

--- src/TTK4147-Project.c
+++ src/TTK4147-Project.c
@@ -13,26 +13,73 @@
 #include <unistd.h>
 #include "TTK4147-Project.h"
 #include "Client.h"
+#include <string.h>
 
+//Variables
+struct udp_conn connection;
+char *ip = "192.168.0.1";
+
+//Functions
 void regulator(){
 
-	puts("Starting PI Regulator");
+
+	float input  = 10;
+	float kp  = 10;
+	float ki  = 10;
+
+	set_u(input);
+	printf("Output %f \n", get_y());
+
+}
+
+float get_y(){
+
+	char get_command[] = "GET";
+	udp_send(&connection, get_command, sizeof(get_command));
+
+	char buffer[25];
+	udp_receive(&connection, buffer, sizeof(buffer)+1);
+
+	return atof(buffer + sizeof("ACKOF: "));
+}
+
+void set_u(float input){
+
+	char set_command[] = "SET:";
+
+	char str_input[sizeof(set_command) + sizeof(input)];
+	sprintf(str_input, "%s%d", set_command, input);
+
+	udp_send(&connection, str_input, sizeof(set_command));
+
+}
+
+void start_server(){
+
+	char start_command[] = "START";
+	udp_send(&connection, start_command, sizeof(start_command));
+
+}
+
+void stop_server(){
+
+	char stop_command[] = "STOP";
+	udp_send(&connection, stop_command, sizeof(stop_command));
 
 }
 
 int main(void) {
-	puts("!!!Hello World!!!");
-	struct udp_conn connection;
-	char ip = "191.168.01";
-	udp_init_client(&connection, 9999, &ip);
+	printf("----- TTK4147 Project ------ \n");
 
+	udp_init_client(&connection, 9999, ip);
+	start_server();
+
+	printf("Starting PI Regulator \n");
 	for(;;){
-
-
-		puts("Running PI Regulator");
-		usleep(100);
+		regulator();
 	}
 
-	regulator();
+	stop_server();
+	udp_close(&connection);
 	return EXIT_SUCCESS;
 }
---
+++ src/TTK4147-Project.h
@@ -0,0 +1,5 @@
+float get_y();
+void regulator();
+void set_u(float input);
+void start_server();
+void stop_server();
