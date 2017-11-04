ca398533c9971b338cb1e4ecb04683bcd86fa282
student <student@Sanntid.(none)>
2017-11-04 16:50:08 +0100

    Changed to semaphores

--- src/Part2.c
+++ src/Part2.c
@@ -13,19 +13,15 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <semaphore.h>
 #include "Client.h"
 #include "Shared.h"
 #include "Queue.h"
 #include "Part2.h"
 
 float part_2_integral = 0.0;
-
-void ack_signal(struct udp_conn *connection){
-
-	char get_command[] = "SIGNAL_ACK";
-	udp_send(connection, get_command, sizeof(get_command));
-
-}
+sem_t sem_y;
+sem_t sem_signal;
 
 void req_y(struct udp_conn *connection){
 
@@ -46,10 +42,10 @@ void *receiver(struct udp_conn *connection){
 		strncpy(type, buffer, 6);
 
 		if (!strncmp(buffer, "SIGNAL", 6)) {
-			queue_add(SIGNAL, 0);
+			sem_post(&sem_signal);
 		}
 		if (!strncmp(buffer, "GET_ACK", 7)) {
-			queue_add(Y, atof(buffer + sizeof("GET_ACK")));
+			sem_post(&sem_y);
 		}
 
 	}
@@ -67,26 +63,29 @@ void *controller(struct udp_conn *connection){
 
 	for (counter = 0; counter < counter_goal; counter++){
 		req_y(connection);
-
-		if(queue.oldest && queue.oldest->type == Y){
-			printf("LOlz \n");
-			struct message* msg = queue_pop();
+		sem_wait(&sem_y);
+			/*
 			printf("%f \n", msg->value);
 			float error = reference - msg->value;
 			printf("Woho, message %f \n", msg->value);
 			part_2_integral = part_2_integral + (error * SLEEP_PERIOD / MILLISEC_TO_SEC);
 			float u = KP * error + KI * part_2_integral;
 			set_u(u);
-		}
-
-		timespec_add_us(&program_sleep, SLEEP_PERIOD);
-		clock_nanosleep(&program_sleep);
+			*/
 	}
 
-}
+	timespec_add_us(&program_sleep, SLEEP_PERIOD);
+	clock_nanosleep(&program_sleep);
 
-void *handler(struct udp_conn *connection){
-	printf("Starting handler \n");
+	}
+
+void *acknowledger(struct udp_conn *connection){
+
+	for(;;){
+		sem_wait(&sem_signal);
+		char get_command[] = "SIGNAL_ACK";
+		udp_send(connection, get_command, sizeof(get_command));
+	}
 
 }
 
@@ -96,18 +95,18 @@ void part_2_main(struct udp_conn *connection){
 	printf("---------- PART 2 ---------- \n");
 
 	// Make sure it can be shared across processes
-	pthread_mutex_init(&queue_mutex, NULL);
-	queue_init();
+	sem_init(&sem_y, 0, 1);
+	sem_init(&sem_signal, 0, 1);
 
     pthread_t thread_receiver;
     pthread_t thread_controller;
-    pthread_t thread_handler;
+    pthread_t thread_acknowledger;
 
     pthread_create(&thread_receiver,  NULL, receiver, connection);
     pthread_create(&thread_controller, NULL, controller, connection);
-    pthread_create(&thread_handler,   NULL, handler, connection);
+    pthread_create(&thread_acknowledger,   NULL, acknowledger, connection);
 
     //Exit after counter
-    pthread_join(thread_controller, NULL);
+    pthread_join(thread_acknowledger, NULL);
 
 }
